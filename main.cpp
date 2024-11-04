e96793726e363fd5fc8ed8261af497979c435345
BYBYYB <1871657079@qq.com>
2024-11-04 21:44:10 +0800

    new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+*******************************************************
+* Created by Niazye, with VSCode, on OMEN blackscreen9*
+*******************************************************
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long long N = 1e6+5;
+void solve()
+{
+    int n;
+    cin >> n;
+
+    return;
+}
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
